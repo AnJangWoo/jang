092597e753a8d32d44228c26b714ad560d91063b
jangdn <jangdn@email.com>
2018-02-05 15:01:10 +0900

    동적 템플릿 추가

--- home_app/views.py
+++ home_app/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
 
-def post_list(request) :
-    return render(request, 'home_app/post_list.html')
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'home_app/post_list.html', {'posts': posts})
